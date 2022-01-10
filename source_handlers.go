150b8e19c6f519d883c6000500f4a5f8a9b178ef
Jacob Lindgren <jlindgre@redhat.com>
2022-01-10 13:59:01 -0600

    add check for cert-auth being used in SourceList handler

--- source_handlers.go
+++ source_handlers.go
@@ -48,6 +48,13 @@ func SourceList(c echo.Context) error {
 		count   int64
 	)
 
+	// When listing sources via cert-auth we want to lock them down to only the
+	// satellite source type.
+	if c.Get("cert-auth") != nil {
+		satlliteId := strconv.Itoa(int(dao.Static.GetSourceTypeId("satellite")))
+		filters = append(filters, util.Filter{Name: "source_type_id", Value: []string{satlliteId}})
+	}
+
 	sources, count, err = sourcesDB.List(limit, offset, filters)
 
 	if err != nil {
