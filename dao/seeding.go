690cc0092d23d86bdcf6c36cb416b6f70ef7abed
Jacob Lindgren <jlindgre@redhat.com>
2022-01-31 13:02:36 -0600

    change using a slice to using a map to make understanding easier

--- dao/seeding.go
+++ dao/seeding.go
@@ -2,10 +2,14 @@ package dao
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"strings"
 
 	l "github.com/RedHatInsights/sources-api-go/logger"
 	m "github.com/RedHatInsights/sources-api-go/model"
+	"github.com/RedHatInsights/sources-api-go/util"
+	"gorm.io/gorm"
 	"sigs.k8s.io/yaml"
 )
 
@@ -50,34 +54,30 @@ func seedSourceTypes(seedDir string) error {
 		return err
 	}
 
-	// load all the seeds from the db
-	sourceTypes := make([]m.SourceType, 0)
-	result := DB.Model(&m.SourceType{}).Scan(&sourceTypes)
-	if result.Error != nil {
-		return result.Error
+	// load the existing source types from the database
+	sourceTypes := loadSourceTypeSeeds()
+	if sourceTypes == nil {
+		return fmt.Errorf("failed to load source type seeds")
 	}
+
+	// the skip list is defined in the environment
+	skipList := strings.Split(os.Getenv("SOURCE_TYPE_SKIP_LIST"), ",")
 
 	// loop through all of the seeds
 	// 1. creating the record if they don't exist
 	// 2. updating the ones that do exist
 	// 3. deleting the ones removed from the list
 	for name, values := range seeds {
-		var st *m.SourceType
-
-		// find an existing one in the list of source types
-		for i, sourceType := range sourceTypes {
-			if sourceType.Name == name {
-				st = &sourceType
-
-				// deleting the sourcetype out of the array since we're handling
-				// it.
-				sourceTypes[i] = sourceTypes[len(sourceTypes)-1]
-				sourceTypes = sourceTypes[:len(sourceTypes)-1]
-				break
-			}
+		if util.SliceContainsString(skipList, name) {
+			l.Log.Infof("Skipping SourceType %v", name)
+			continue
 		}
 
-		// if the source type was not found - create it
+		// find the source type in the hash - delete is a no-op if it is not there.
+		st := sourceTypes[name]
+		delete(sourceTypes, name)
+
+		// if the source type was not found - we are creating it
 		if st == nil {
 			l.Log.Debugf("New SourceType found %v", name)
 			st = &m.SourceType{}
@@ -95,6 +95,7 @@ func seedSourceTypes(seedDir string) error {
 		st.Vendor = values.Vendor
 		st.Name = name
 
+		var result *gorm.DB
 		// if this is a new record we need to create rather than save.
 		if st.Id == 0 {
 			result = DB.Create(&st)
@@ -109,9 +110,9 @@ func seedSourceTypes(seedDir string) error {
 
 	// if there were any sourcetypes left - they were removed from the seed file
 	// and need deleted
-	for _, sourceType := range sourceTypes {
-		l.Log.Infof("Deleting SourceType %v", sourceType.Name)
-		result := DB.Delete(&sourceType)
+	for name := range sourceTypes {
+		l.Log.Infof("Deleting SourceType %v", name)
+		result := DB.Delete(sourceTypes[name])
 		if result.Error != nil {
 			return result.Error
 		}
@@ -132,30 +133,26 @@ func seedApplicationTypes(seedDir string) error {
 		return err
 	}
 
-	// load all the seeds from the db
-	appTypes := make([]m.ApplicationType, 0)
-	result := DB.Model(&m.ApplicationType{}).Scan(&appTypes)
-	if result.Error != nil {
-		return result.Error
+	// load the existing application types from the database
+	appTypes := loadApplicationTypeSeeds()
+	if appTypes == nil {
+		return fmt.Errorf("failed to load application type seeds")
 	}
 
+	// the skip list is defined in the environment
+	skipList := strings.Split(os.Getenv("APPLICATION_TYPE_SKIP_LIST"), ",")
+
 	for name, values := range seeds {
-		var at *m.ApplicationType
-
-		// find an existing one in the list of application types
-		for i, appType := range appTypes {
-			if appType.Name == name {
-				at = &appType
-
-				// deleting the apptype out of the array since we're handling
-				// it.
-				appTypes[i] = appTypes[len(appTypes)-1]
-				appTypes = appTypes[:len(appTypes)-1]
-				break
-			}
+		if util.SliceContainsString(skipList, name) {
+			l.Log.Infof("Skipping ApplicationType %v", name)
+			continue
 		}
 
-		// if the app type was not found - create it
+		// find the application type in the hash - delete is a no-op if it is not there.
+		at := appTypes[name]
+		delete(appTypes, name)
+
+		// if the app type was not found - we are creating it
 		if at == nil {
 			l.Log.Debugf("New ApplicationType found %v", name)
 			at = &m.ApplicationType{}
@@ -182,6 +179,7 @@ func seedApplicationTypes(seedDir string) error {
 		at.DisplayName = values.DisplayName
 		at.Name = name
 
+		var result *gorm.DB
 		// if this is a new record we need to create rather than save.
 		if at.Id == 0 {
 			result = DB.Create(&at)
@@ -196,9 +194,9 @@ func seedApplicationTypes(seedDir string) error {
 
 	// if there were any applicationtypes left - they were removed from the seed file
 	// and need deleted
-	for _, appType := range appTypes {
-		l.Log.Infof("Deleting ApplicationType %v", appType.Name)
-		result := DB.Delete(&appType)
+	for name := range appTypes {
+		l.Log.Infof("Deleting ApplicationType %v", name)
+		result := DB.Delete(appTypes[name])
 		if result.Error != nil {
 			return result.Error
 		}
@@ -325,4 +323,41 @@ func seedAppMetadata(seedDir string) error {
 	}
 
 	return nil
+}
+
+// loads all the data from the source_types table into a map of the name and a
+// pointer to the record
+func loadSourceTypeSeeds() map[string]*m.SourceType {
+	sourceTypes := make([]m.SourceType, 0)
+	result := DB.Model(&m.SourceType{}).Scan(&sourceTypes)
+	if result.Error != nil {
+		return nil
+	}
+
+	// parse them into a map with keys name and value db type
+	hash := make(map[string]*m.SourceType)
+	for i, sourceType := range sourceTypes {
+		hash[sourceType.Name] = &sourceTypes[i]
+	}
+
+	return hash
+}
+
+// loads all the data from the source_types table into a map of the name and a
+// pointer to the record
+func loadApplicationTypeSeeds() map[string]*m.ApplicationType {
+	// load all the seeds from the db
+	appTypes := make([]m.ApplicationType, 0)
+	result := DB.Model(&m.ApplicationType{}).Scan(&appTypes)
+	if result.Error != nil {
+		return nil
+	}
+
+	// parse them into a map with keys name and value db type
+	hash := make(map[string]*m.ApplicationType)
+	for i, appType := range appTypes {
+		hash[appType.Name] = &appTypes[i]
+	}
+
+	return hash
 }
