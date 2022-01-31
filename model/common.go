80d3f0c2b754f7ec85da30f84d0a3422c5595e48
Petra Cihalova <pcihalov@redhat.com>
2022-01-31 21:49:16 +0100

    add new structs PauseResponse and AvailabilityStatusResponse

--- model/common.go
+++ model/common.go
@@ -2,11 +2,6 @@ package model
 
 import "time"
 
-type TimeStamps struct {
-	CreatedAt *time.Time `json:"created_at"`
-	UpdatedAt *time.Time `json:"updated_at"`
-}
-
 type Pause struct {
 	PausedAt time.Time `json:"paused_at,omitempty"`
 }
@@ -15,4 +10,14 @@ type AvailabilityStatus struct {
 	AvailabilityStatus string    `json:"availability_status,omitempty"`
 	LastCheckedAt      time.Time `json:"last_checked_at,omitempty"`
 	LastAvailableAt    time.Time `json:"last_available_at,omitempty"`
+}
+
+type PauseResponse struct {
+	PausedAt *string `json:"paused_at,omitempty"`
+}
+
+type AvailabilityStatusResponse struct {
+	AvailabilityStatus *string `json:"availability_status,omitempty"`
+	LastCheckedAt      *string `json:"last_checked_at,omitempty"`
+	LastAvailableAt    *string `json:"last_available_at,omitempty"`
 }
