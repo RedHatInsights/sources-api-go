8cbeee0c49d3a2762bd1b6a90ed6ce461868b7aa
MikelAlejoBR <mbarcina@redhat.com>
2022-02-04 07:45:35 +0100

    refactor: move internal handler to a separate file

---
+++ internal_handlers_test.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/RedHatInsights/sources-api-go/internal/testutils/fixtures"
+	"github.com/RedHatInsights/sources-api-go/internal/testutils/request"
+	"github.com/RedHatInsights/sources-api-go/util"
+)
+
+func TestSourceListInternal(t *testing.T) {
+	if !flags.Integration {
+		t.Skip("Only run during integration tests")
+	}
+
+	c, rec := request.CreateTestContext(
+		http.MethodGet,
+		"/internal/v2.0/sources",
+		nil,
+		map[string]interface{}{
+			"limit":   100,
+			"offset":  0,
+			"filters": []util.Filter{},
+		})
+
+	err := InternalSourceList(c)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if rec.Code != 200 {
+		t.Error("Did not return 200")
+	}
+
+	var out util.Collection
+	err = json.Unmarshal(rec.Body.Bytes(), &out)
+	if err != nil {
+		t.Error("Failed unmarshalling output")
+	}
+
+	if out.Meta.Limit != 100 {
+		t.Error("limit not set correctly")
+	}
+
+	if out.Meta.Offset != 0 {
+		t.Error("offset not set correctly")
+	}
+
+	if len(out.Data) != len(fixtures.TestSourceData) {
+		t.Error("not enough objects passed back from DB")
+	}
+
+	for i, src := range out.Data {
+		s, ok := src.(map[string]interface{})
+		if !ok {
+			t.Error("model did not deserialize as a source")
+		}
+
+		// Parse the source
+		responseSourceId, err := util.InterfaceToInt64(s["id"])
+		if err != nil {
+			t.Errorf("could not parse id from response: %s", err)
+		}
+
+		responseExternalTenant := s["tenant"].(string)
+		responseAvailabilityStatus := s["availability_status"].(string)
+
+		// Check that the expected source data and the received data are the same
+		if want := fixtures.TestSourceData[i].ID; want != responseSourceId {
+			t.Errorf("Ids don't match. Want %d, got %d", want, responseSourceId)
+		}
+
+		if want := fixtures.TestTenantData[0].ExternalTenant; want != responseExternalTenant {
+			t.Errorf("Tenants don't match. Want %#v, got %#v", want, responseExternalTenant)
+		}
+
+		if want := fixtures.TestSourceData[i].AvailabilityStatus.AvailabilityStatus; want != responseAvailabilityStatus {
+			t.Errorf("Availability statuses don't match. Want %s, got %s", want, responseAvailabilityStatus)
+		}
+	}
+
+	AssertLinks(t, c.Request().RequestURI, out.Links, 100, 0)
+}
--- source_handlers.go
+++ source_handlers.go
@@ -325,30 +325,3 @@ func SourceCheckAvailability(c echo.Context) error {
 
 	return c.JSON(http.StatusAccepted, map[string]interface{}{})
 }
-
-func InternalSourceList(c echo.Context) error {
-	filters, err := getFilters(c)
-	if err != nil {
-		return err
-	}
-
-	limit, offset, err := getLimitAndOffset(c)
-	if err != nil {
-		return err
-	}
-
-	// The DAO doesn't need a tenant set, since the queries won't be filtered by that tenant
-	sourcesDB := &dao.SourceDaoImpl{}
-	sources, count, err := sourcesDB.ListInternal(limit, offset, filters)
-
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, util.ErrorDoc(err.Error(), "400"))
-	}
-
-	out := make([]interface{}, len(sources))
-	for i := 0; i < len(sources); i++ {
-		out[i] = sources[i].ToInternalResponse()
-	}
-
-	return c.JSON(http.StatusOK, util.CollectionResponse(out, c.Request(), int(count), limit, offset))
-}
