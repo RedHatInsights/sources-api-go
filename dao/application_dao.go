f9e8f583c88d75657bc03a33a74134fd842eccaf
Jacob Lindgren <jlindgre@redhat.com>
2022-04-20 10:11:44 -0500

    Update Application DAO List to explicitly specify tenant_id relation

--- dao/application_dao.go
+++ dao/application_dao.go
@@ -57,8 +57,9 @@ func (a *applicationDaoImpl) SubCollectionList(primaryCollection interface{}, li
 
 func (a *applicationDaoImpl) List(limit int, offset int, filters []util.Filter) ([]m.Application, int64, error) {
 	applications := make([]m.Application, 0, limit)
-	query := DB.Debug().Model(&m.Application{}).
-		Where("tenant_id = ?", a.TenantID)
+	query := DB.Debug().
+		Model(&m.Application{}).
+		Where("applications.tenant_id = ?", a.TenantID)
 
 	query, err := applyFilters(query, filters)
 	if err != nil {
@@ -68,7 +69,10 @@ func (a *applicationDaoImpl) List(limit int, offset int, filters []util.Filter) 
 	count := int64(0)
 	query.Count(&count)
 
-	result := query.Limit(limit).Offset(offset).Find(&applications)
+	result := query.
+		Limit(limit).
+		Offset(offset).
+		Find(&applications)
 	if result.Error != nil {
 		return nil, 0, util.NewErrBadRequest(result.Error)
 	}
