ef47d7d8f2b324c7b24efa859053ee9688986755
Libor Pichler <lpichler@redhat.com>
2022-05-25 10:52:56 +0200

    Fix: skip feature flags when its acg config is missing in config

--- config/config.go
+++ config/config.go
@@ -87,21 +87,21 @@ func Get() *SourcesApiConfig {
 		options.SetDefault("CachePort", cfg.InMemoryDb.Port)
 		options.SetDefault("CachePassword", cfg.InMemoryDb.Password)
 
-		options.SetDefault("FeatureFlagsHost", cfg.FeatureFlags.Hostname)
-		options.SetDefault("FeatureFlagsPort", cfg.FeatureFlags.Port)
-		options.SetDefault("FeatureFlagsSchema", string(cfg.FeatureFlags.Scheme))
+		if cfg.FeatureFlags != nil {
+			unleashUrl := ""
+			if cfg.FeatureFlags.Hostname != "" && cfg.FeatureFlags.Port != 0 && cfg.FeatureFlags.Scheme != "" {
+				unleashUrl = fmt.Sprintf("%s://%s:%d/api", cfg.FeatureFlags.Scheme, cfg.FeatureFlags.Hostname, cfg.FeatureFlags.Port)
+			}
 
-		unleashUrl := ""
-		if cfg.FeatureFlags.Hostname != "" {
-			unleashUrl = fmt.Sprintf("%s://%s:%d/api", cfg.FeatureFlags.Scheme, cfg.FeatureFlags.Hostname, cfg.FeatureFlags.Port)
-		}
-		options.SetDefault("FeatureFlagsUrl", unleashUrl)
+			options.SetDefault("FeatureFlagsUrl", unleashUrl)
 
-		clientAccessToken := ""
-		if cfg.FeatureFlags.ClientAccessToken != nil {
-			clientAccessToken = *cfg.FeatureFlags.ClientAccessToken
+			clientAccessToken := ""
+			if cfg.FeatureFlags.ClientAccessToken != nil {
+				clientAccessToken = *cfg.FeatureFlags.ClientAccessToken
+			}
+			options.SetDefault("FeatureFlagsBearerToken", clientAccessToken)
 		}
-		options.SetDefault("FeatureFlagsBearerToken", clientAccessToken)
+
 	} else {
 		options.SetDefault("AwsRegion", "us-east-1")
 		options.SetDefault("AwsAccessKeyId", os.Getenv("CW_AWS_ACCESS_KEY_ID"))
