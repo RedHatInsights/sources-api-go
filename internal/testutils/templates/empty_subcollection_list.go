8537bd53501e1ae80b4b36a5db577f71d63d484a
Petra Cihalova <pcihalov@redhat.com>
2022-06-16 08:18:24 +0200

    added new test template for empty subcollection list test

---
+++ internal/testutils/templates/empty_subcollection_list.go
@@ -0,0 +1,37 @@
+package templates
+
+import (
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/RedHatInsights/sources-api-go/util"
+)
+
+func EmptySubcollectionListTest(t *testing.T, rec *httptest.ResponseRecorder) {
+	if rec.Code != 200 {
+		t.Error("Did not return 200")
+	}
+
+	var out util.Collection
+	err := json.Unmarshal(rec.Body.Bytes(), &out)
+	if err != nil {
+		t.Error("Failed unmarshaling output")
+	}
+
+	if out.Meta.Limit != 100 {
+		t.Error("limit not set correctly")
+	}
+
+	if out.Meta.Offset != 0 {
+		t.Error("offset not set correctly")
+	}
+
+	if out.Meta.Count != 0 {
+		t.Error("count not set correctly")
+	}
+
+	if len(out.Data) != 0 {
+		t.Errorf("expected 0 objects passed back from DB, got %d", len(out.Data))
+	}
+}
