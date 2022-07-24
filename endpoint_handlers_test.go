7ae20ea5c6710582f4afeb571c8e1a2ae2a8f890
Petra Cihalova <pcihalov@redhat.com>
2022-07-24 18:32:04 +0200

    added test for SourceListEndpoint() and a source without endpoints

--- endpoint_handlers_test.go
+++ endpoint_handlers_test.go
@@ -117,6 +117,35 @@ func TestSourceEndpointSubcollectionList(t *testing.T) {
 	testutils.AssertLinks(t, c.Request().RequestURI, out.Links, 100, 0)
 }
 
+// TestSourceEndpointSubcollectionListEmptyList tests that empty list is
+// returned for source id without endpoints
+func TestSourceEndpointSubcollectionListEmptyList(t *testing.T) {
+	tenantId := int64(1)
+	sourceId := int64(101)
+
+	c, rec := request.CreateTestContext(
+		http.MethodGet,
+		"/api/sources/v3.1/sources/1/endpoints",
+		nil,
+		map[string]interface{}{
+			"limit":    100,
+			"offset":   0,
+			"filters":  []util.Filter{},
+			"tenantID": tenantId,
+		},
+	)
+
+	c.SetParamNames("source_id")
+	c.SetParamValues(fmt.Sprintf("%d", sourceId))
+
+	err := SourceListEndpoint(c)
+	if err != nil {
+		t.Error(err)
+	}
+
+	templates.EmptySubcollectionListTest(t, c, rec)
+}
+
 func TestSourceEndpointSubcollectionListNotFound(t *testing.T) {
 	c, rec := request.CreateTestContext(
 		http.MethodGet,
