ad1fefe274be27e04a3a77ddc93dee1ccca71606
MikelAlejoBR <mbarcina@redhat.com>
2022-08-18 17:02:44 +0200

    refactor: tenancy middleware sets the tenant data from the database
    Instead of just forwarding the incoming headers, this change ensures
    that the "EBS account number" and the "OrgId" are set in the context
    using the contents from the database.
    
    This comes from one issue we have been having with the notifications
    service: the service was erroring because we were just sending the
    incoming "EBS account number" header, even though we had the associated
    "OrgId" on the database.
    
    So we have decided to simply use the incoming headers as a validation
    —make sure that an EBS account number and/or OrgId are present—, and
    then use the database contents to send the headers to the rest of the
    services or to the Kafka queues.

--- middleware/tenancy.go
+++ middleware/tenancy.go
@@ -33,10 +33,6 @@ func Tenancy(next echo.HandlerFunc) echo.HandlerFunc {
 			}
 		}
 
-		// Store the EBS account number and the OrgId in the context for easier usage later.
-		c.Set(h.ACCOUNT_NUMBER, id.Identity.AccountNumber)
-		c.Set(h.ORGID, id.Identity.OrgID)
-
 		c.Logger().Debugf("[org_id: %s][account_number: %s] Looking up Tenant ID", id.Identity.OrgID, id.Identity.AccountNumber)
 
 		tenantDao := dao.GetTenantDao()
@@ -45,7 +41,16 @@ func Tenancy(next echo.HandlerFunc) echo.HandlerFunc {
 			return fmt.Errorf("failed to get or create tenant for request: %s", err)
 		}
 
+		// Store the ID, EBS account number and OrgId from what we've got in the database. Prior to this, we stored
+		// the contents of the incoming headers, but this had a problem: if we only received an EBS account number, we
+		// would only forward that account number even if we had a complementary OrgId number stored.
+		//
+		// This can cause issues with services we are integrated with —notifications, for example—, which will not
+		// accept EBS account numbers anymore. However, we can deal with that by forwarding the OrgId too if we have it
+		// stored in the database.
 		c.Set(h.TENANTID, tenant.Id)
+		c.Set(h.ACCOUNT_NUMBER, tenant.ExternalTenant)
+		c.Set(h.ORGID, tenant.OrgID)
 
 		return next(c)
 	}
