01b50b7b4e3bf277e0addb7ef60b67307b82a7b1
Petra Cihalova <pcihalov@redhat.com>
2022-08-29 22:27:41 +0200

    added helper function ElementsInSlicesEqual() which sorts and compares elements in two int64 slices

--- util/slice_utils.go
+++ util/slice_utils.go
@@ -1,5 +1,11 @@
 package util
 
+import (
+	"sort"
+
+	"github.com/google/go-cmp/cmp"
+)
+
 // SliceContainsString returns true if the specified target is present in the given slice.
 func SliceContainsString(slice []string, target string) bool {
 	for _, element := range slice {
@@ -8,4 +14,12 @@ func SliceContainsString(slice []string, target string) bool {
 		}
 	}
 	return false
+}
+
+// ElementsInSlicesEqual sorts and compare slices of int64 and returns that slices are equal
+func ElementsInSlicesEqual(sliceA []int64, sliceB []int64) bool {
+	sort.Slice(sliceA, func(i, j int) bool { return sliceA[i] < sliceA[j] })
+	sort.Slice(sliceB, func(i, j int) bool { return sliceB[i] < sliceB[j] })
+
+	return cmp.Equal(sliceA, sliceB)
 }
