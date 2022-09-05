a03f59acb25c4bb58a5cc037a1b4a8e3e73d5d6a
Petra Cihalova <pcihalov@redhat.com>
2022-09-05 17:36:19 +0200

    refactor: mock app auth dao refactor - unified receivers names and changed unused param into underscore

--- internal/testutils/mocks/mock_dao_application_authentication.go
+++ internal/testutils/mocks/mock_dao_application_authentication.go
@@ -9,13 +9,13 @@ type MockApplicationAuthenticationDao struct {
 	ApplicationAuthentications []m.ApplicationAuthentication
 }
 
-func (m MockApplicationAuthenticationDao) List(limit, offset int, filters []util.Filter) ([]m.ApplicationAuthentication, int64, error) {
-	count := int64(len(m.ApplicationAuthentications))
-	return m.ApplicationAuthentications, count, nil
+func (mockAppAuthDao MockApplicationAuthenticationDao) List(_, _ int, _ []util.Filter) ([]m.ApplicationAuthentication, int64, error) {
+	count := int64(len(mockAppAuthDao.ApplicationAuthentications))
+	return mockAppAuthDao.ApplicationAuthentications, count, nil
 }
 
-func (m MockApplicationAuthenticationDao) GetById(id *int64) (*m.ApplicationAuthentication, error) {
-	for _, appAuth := range m.ApplicationAuthentications {
+func (mockAppAuthDao MockApplicationAuthenticationDao) GetById(id *int64) (*m.ApplicationAuthentication, error) {
+	for _, appAuth := range mockAppAuthDao.ApplicationAuthentications {
 		if appAuth.ID == *id {
 			return &appAuth, nil
 		}
@@ -24,23 +24,23 @@ func (m MockApplicationAuthenticationDao) GetById(id *int64) (*m.ApplicationAuth
 	return nil, util.NewErrNotFound("application authentication")
 }
 
-func (m MockApplicationAuthenticationDao) Create(src *m.ApplicationAuthentication) error {
+func (mockAppAuthDao MockApplicationAuthenticationDao) Create(_ *m.ApplicationAuthentication) error {
 	return nil
 }
 
-func (m MockApplicationAuthenticationDao) Update(src *m.ApplicationAuthentication) error {
+func (mockAppAuthDao MockApplicationAuthenticationDao) Update(_ *m.ApplicationAuthentication) error {
 	panic("implement me")
 }
 
-func (m MockApplicationAuthenticationDao) Delete(id *int64) (*m.ApplicationAuthentication, error) {
-	return m.GetById(id)
+func (mockAppAuthDao MockApplicationAuthenticationDao) Delete(id *int64) (*m.ApplicationAuthentication, error) {
+	return mockAppAuthDao.GetById(id)
 }
 
-func (m MockApplicationAuthenticationDao) Tenant() *int64 {
+func (mockAppAuthDao MockApplicationAuthenticationDao) Tenant() *int64 {
 	tenant := int64(1)
 	return &tenant
 }
 
-func (m MockApplicationAuthenticationDao) ApplicationAuthenticationsByResource(_ string, _ []m.Application, _ []m.Authentication) ([]m.ApplicationAuthentication, error) {
-	return m.ApplicationAuthentications, nil
+func (mockAppAuthDao MockApplicationAuthenticationDao) ApplicationAuthenticationsByResource(_ string, _ []m.Application, _ []m.Authentication) ([]m.ApplicationAuthentication, error) {
+	return mockAppAuthDao.ApplicationAuthentications, nil
 }
