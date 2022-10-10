74f038c7b34b96e429affa7d2078ab65aa62ed00
Petra Cihalova <pcihalov@redhat.com>
2022-10-10 15:46:51 +0200

    added tests for parseSources() in bulk_create.go when source type name is in req

--- service/bulk_create_test.go
+++ service/bulk_create_test.go
@@ -133,8 +133,9 @@ func TestParseEndpointsTestRegression(t *testing.T) {
 	}
 }
 
-// TestParseSources tests that correct output is returned for valid inputs
-func TestParseSources(t *testing.T) {
+// TestParseSourcesWithSourceTypeId tests that correct output is returned for valid inputs
+// and with source type id
+func TestParseSourcesWithSourceTypeId(t *testing.T) {
 	testutils.SkipIfNotRunningIntegrationTests(t)
 
 	// Prepare test data
@@ -146,6 +147,61 @@ func TestParseSources(t *testing.T) {
 				Name:            util.StringRef(sourceName),
 				SourceTypeIDRaw: sourceTypeFixture.Id,
 			},
+		},
+	}
+
+	tenant := fixtures.TestTenantData[0]
+	userResource := model.UserResource{}
+
+	// Parse the sources
+	var err error
+	sources, err := parseSources(reqSources, &tenant, &userResource)
+	if err != nil {
+		t.Errorf(`unexpected error when parsing the sources from bulk create: %s`, err)
+	}
+
+	// Check the results
+	if len(sources) != 1 {
+		t.Errorf("expected 1 source returned from parseSources() but got %d", len(sources))
+	}
+
+	sourceOut := sources[0]
+
+	if sourceOut.AvailabilityStatus != model.InProgress {
+		t.Errorf("expected availability status 'in_progress', got %s", sourceOut.AvailabilityStatus)
+	}
+
+	if sourceOut.SourceTypeID != sourceTypeFixture.Id {
+		t.Errorf("expected source type id %d, got %d", sourceTypeFixture.Id, sourceOut.SourceTypeID)
+	}
+
+	if sourceOut.Name != sourceName {
+		t.Errorf("expected source name %s, got %s", sourceName, sourceOut.Name)
+	}
+
+	if sourceOut.TenantID != tenant.Id {
+		t.Errorf("expected tenant id %d, got %d", tenant.Id, sourceOut.TenantID)
+	}
+
+	if sourceOut.UserID != nil {
+		t.Errorf("expected user id = nil, got %d", sourceOut.UserID)
+	}
+}
+
+// TestParseSourcesWithSourceTypeName tests that correct output is returned for valid inputs
+// and with source type name
+func TestParseSourcesWithSourceTypeName(t *testing.T) {
+	testutils.SkipIfNotRunningIntegrationTests(t)
+
+	// Prepare test data
+	sourceTypeFixture := fixtures.TestSourceTypeData[1]
+	sourceName := "Source for TestParseSources()"
+	var reqSources = []model.BulkCreateSource{
+		{
+			SourceCreateRequest: model.SourceCreateRequest{
+				Name: util.StringRef(sourceName),
+			},
+			SourceTypeName: sourceTypeFixture.Name,
 		},
 	}
 
@@ -244,6 +300,38 @@ func TestParseSourcesNotFoundInvalidSourceTypeId(t *testing.T) {
 	sources, err := parseSources(reqSources, &tenant, &userResource)
 	if !errors.Is(err, util.ErrNotFoundEmpty) {
 		t.Errorf("expected not found error, got <%s>", err)
+	}
+
+	if sources != nil {
+		t.Error("ghost infected the return")
+	}
+}
+
+// TestParseSourcesBadRequestInvalidSourceTypeName tests that bad request is returned
+// for invalid source type name
+func TestParseSourcesBadRequestInvalidSourceTypeName(t *testing.T) {
+	testutils.SkipIfNotRunningIntegrationTests(t)
+
+	// Prepare test data
+	sourceTypeName := "invalid name"
+	sourceName := "Source for TestParseSources()"
+	var reqSources = []model.BulkCreateSource{
+		{
+			SourceCreateRequest: model.SourceCreateRequest{
+				Name: util.StringRef(sourceName),
+			},
+			SourceTypeName: sourceTypeName,
+		},
+	}
+
+	tenant := fixtures.TestTenantData[0]
+	userResource := model.UserResource{}
+
+	// Parse the sources and check the results
+	var err error
+	sources, err := parseSources(reqSources, &tenant, &userResource)
+	if !errors.Is(err, util.ErrBadRequestEmpty) {
+		t.Errorf("expected bad request error, got <%s>", err)
 	}
 
 	if sources != nil {
