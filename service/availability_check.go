cdd2714f27dd38f4204aca4cad5f4ef417318c50
Jacob Lindgren <jlindgre@redhat.com>
2022-10-27 15:59:33 -0500

    check both rhc + source for availability status changes

--- service/availability_check.go
+++ service/availability_check.go
@@ -294,8 +294,8 @@ func (acr availabilityCheckRequester) pingRHC(source *m.Source, rhcConnection *m
 		return
 	}
 
-	// only go through and update if there was a change.
-	if rhcConnection.AvailabilityStatus != sanitizedStatus {
+	// only go through and update if there was a change. to either the source or rhc connection
+	if rhcConnection.AvailabilityStatus != sanitizedStatus || source.AvailabilityStatus != sanitizedStatus {
 		acr.updateRhcStatus(source, sanitizedStatus, errstr, rhcConnection, headers)
 	}
 }
