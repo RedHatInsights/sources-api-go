48594a298b2a17365153ea142b8205a78f53f429
Petra Číhalová <pcihalov@redhat.com>
2024-07-17 22:23:33 +0200

    upgrade go version into 1.21

--- pr_check.sh
+++ pr_check.sh
@@ -55,7 +55,7 @@ export DATABASE_NAME=sources_api_test_go
 
 echo "Running tests...here we go"
 
-export GOROOT="/opt/go/1.20.10"
+export GOROOT="/opt/go/1.21.3"
 export PATH="${GOROOT}/bin:${PATH}"
 make alltest
 
