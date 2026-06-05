RedHatInsights/sources-api-go

commit fbb4fc892a3772f85d8b144411cea226bbccb33d

flesh out gorm logger methods

logger/gorm_logger.go
@@ -19,14 +19,6 @@ type GormLogger struct {
 
 func (l *GormLogger) LogMode(gormLogger.LogLevel) gormLogger.Interface { return l }
 
-// Functions implementing the rest of the gorm logger interface - not used
-// unless you were to call `DB.Logger.Info(...)`, which we don't use. Everything
-// else goes through Trace
-func (l *GormLogger) Debug(_ context.Context, logMessage string, data ...interface{}) {}
-func (l *GormLogger) Info(_ context.Context, logMessage string, data ...interface{})  {}
-func (l *GormLogger) Warn(_ context.Context, logMessage string, data ...interface{})  {}
-func (l *GormLogger) Error(_ context.Context, logMessage string, data ...interface{}) {}
-
 // Trace runs a SQL query and logs how long it took as well as the sql executed.
 // By default the log entry is debug, but if the SQL is very slow it will log as
 // warn.
@@ -36,16 +28,7 @@ func (l *GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql 
 	duration := float64(elapsed.Nanoseconds()) / 1e6
 	fileWithLineNum := utils.FileWithLineNum()
 
-	var entry *logrus.Entry
-	var ok bool
-	if ctx.Value(EchoLogger{}) != nil {
-		if entry, ok = ctx.Value(EchoLogger{}).(*logrus.Entry); !ok {
-			// falling back to a default logger if the asserion fails
-			entry = Log.WithFields(logrus.Fields{})
-		}
-	} else {
-		entry = Log.WithFields(logrus.Fields{})
-	}
+	entry := getEntryFromContext(ctx)
 
 	sqlFields := logrus.Fields{
 		"rows":     rows,
@@ -65,4 +48,34 @@ func (l *GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql 
 	default:
 		entry.WithFields(sqlFields).Debug(sql)
 	}
+}
+
+// Functions implementing the rest of the gorm logger interface - not used
+// unless you were to call `DB.Logger.Info(...)`, which we don't use. Everything
+// else goes through Trace
+func (l *GormLogger) Debug(ctx context.Context, logMessage string, data ...interface{}) {
+	getEntryFromContext(ctx).Debugf(logMessage, data...)
+}
+
+func (l *GormLogger) Info(ctx context.Context, logMessage string, data ...interface{}) {
+	getEntryFromContext(ctx).Infof(logMessage, data...)
+}
+
+func (l *GormLogger) Warn(ctx context.Context, logMessage string, data ...interface{}) {
+	getEntryFromContext(ctx).Warnf(logMessage, data...)
+}
+
+func (l *GormLogger) Error(ctx context.Context, logMessage string, data ...interface{}) {
+	getEntryFromContext(ctx).Errorf(logMessage, data...)
+}
+
+func getEntryFromContext(ctx context.Context) *logrus.Entry {
+	if ctx != nil && ctx.Value(EchoLogger{}) != nil {
+		if entry, ok := ctx.Value(EchoLogger{}).(*logrus.Entry); ok {
+			return entry
+		}
+	}
+
+	// falling back to a default logger if the asserion fails
+	return Log.WithFields(logrus.Fields{})
 }

commit 1f69e8dd87fa8a83d7651dd1148c9fef65a7bf35

Fix spelling of assertion
Introduced by fbb4fc892a3772f85d8b144411cea226bbccb33d

logger/gorm_logger.go
@@ -76,6 +76,6 @@ func getEntryFromContext(ctx context.Context) *logrus.Entry {
 		}
 	}
 
-	// falling back to a default logger if the asserion fails
+	// falling back to a default logger if the assertion fails
 	return Log.WithFields(logrus.Fields{})
 }