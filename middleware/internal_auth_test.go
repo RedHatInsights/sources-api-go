84f6d795c9acc44b3f6b340aa3acffbd1b02a97d
Dalia Cervantes <dcervant@redhat.com>
2026-07-09 09:31:55 -0500

    fix: Remove unused util import from tests

--- middleware/internal_auth_test.go
+++ middleware/internal_auth_test.go
@@ -8,7 +8,6 @@ import (
 	"testing"
 
 	h "github.com/RedHatInsights/sources-api-go/middleware/headers"
-	"github.com/RedHatInsights/sources-api-go/util"
 	"github.com/labstack/echo/v4"
 	"github.com/redhatinsights/platform-go-middlewares/v2/identity"
 )
